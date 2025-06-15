05a42bb31a36acc73acb2b8b0f77d04bdb8f6fe2
Prithivi36 <prithivipappian@gmail.com>
2025-06-15 20:22:44 +0530

    drag and Drop

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -3,9 +3,11 @@ import './App.css'
 import targImg from "./static/target.svg"
 import WordLib from "./WordLib";
 import Targets from "./Targets";
+import Word from "./Word";
 
 function Canvas() {
     const [target,setTarget]=React.useState(false);
+    const [dropped,setDropped]=React.useState([]);
   return (
     <>
     <div className=" row m-0 vh-100 text-light">
@@ -13,10 +15,36 @@ function Canvas() {
             {target && 
                 <Targets setTarget={setTarget} />
             }
-            <div className="bg-black canvas col-12 col-md">
+            <div className="bg-black canvas col-12 col-md"
+                onDragOver={(e)=>e.preventDefault()}
+                onDrop={(e)=>{
+                    e.preventDefault()
+                    const data = e.dataTransfer.getData("text/plain")
+                    
+                    const x = e.clientX;
+                    const y = e.clientY;
+
+                    setDropped((prev)=> [...prev,{data,x,y}]);
+
+                    console.log(dropped);
+                }}
+            >
                 {!target && <button onClick={()=>setTarget(true)} className="target-toggler-open btn rounded-5 btn-outline-light">
                     <i className="bi bullseye bi-bullseye"></i>
                     </button>}
+                    {dropped.map((item, index) => (
+                        <div
+                            key={index}
+                            style={{
+                            position: 'absolute',
+                            top: item.y,
+                            left: item.x,
+                            transform: 'translate(-50%, -50%)',
+                            }}
+                        >
+                            {<Word w={item.data}/>}
+                        </div>
+                    ))}
             </div>
             <WordLib />
     </div>
--- src/WordLib.jsx
+++ src/WordLib.jsx
@@ -1,8 +1,67 @@
+import Word from "./Word"
+
 function WordLib() {
+    const words =[
+        "apple",
+        "brisk",
+        "canvas",
+        "delta",
+        "echo",
+        "frost",
+        "glimmer",
+        "hollow",
+        "ignite",
+        "jungle",
+        "frost",
+        "lunar",
+        "mystic",
+        "canvas",
+        "delta",
+        "mystic",
+        "echo",
+        "glimmer",
+        "hollow",
+        "ignite",
+        "jungle",
+        "knight",
+        "lunar",
+        "canvas",
+        "delta",
+        "echo",
+        "glimmer",
+        "knight",
+        "frost",
+        "hollow",
+        "prism",
+        "ignite",
+        "jungle",
+        "knight",
+        "lunar",
+        "mystic",
+        "nebula",
+        "oasis",
+        "quartz",
+        "ripple",
+        "solace",
+        "thrive",
+        "umbra",
+        "velvet",
+        "whisper",
+        "xenon",
+        "yonder",
+        "zephyr"
+    ]
   return (
     <div className="col-3 d-flex flex-column bg-black p-md-5 gap-4 word-lib p-4 ">
-        <div className="bg-glass rounded-5 h-100">
-            
+        <div className="bg-glass row py-3 gap-2 px-2 word-container rounded-5 h-100">
+
+            {
+                words.map((w,i)=>{
+                    return (
+                        <Word key={i} w={w} />
+                    )
+                })
+            }
         </div>
         <div className="w-100 search-comp d-flex align-items-center justify-content-center">
             <input type="text" className="search rounded-5 p-1 p-md-2 px-3 placeholder-wave text-light bg-black" placeholder={"Search for word..."} />
